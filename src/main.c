745e466467210d31ce6eb25f109adadb244dab83
TonyWu20 <tony.w21@gmail.com>
2021-10-10 03:27:25 +0800

    Add testing for parsing base model .msi

--- src/main.c
+++ src/main.c
@@ -24,4 +24,15 @@ int main(int argc, char *argv[])
         printf("Atom %d %s XYZ: %lf, %lf, %lf\n", atoms[i].itemId, atoms[i].elm,
                atoms[i].coord[0], atoms[i].coord[1], atoms[i].coord[2]);
     }
+    fclose(file);
+    BASE_LATTICE model;
+    fileName = "SAC_GDY_V.msi";
+    file = fopen(fileName, "r");
+    model = parseBase(file);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Vector: %lf, %lf, %lf\n", model.latVector[i][0],
+               model.latVector[i][1], model.latVector[i][2]);
+    }
+    fclose(file);
 }
