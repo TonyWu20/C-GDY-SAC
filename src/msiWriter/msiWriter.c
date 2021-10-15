5868e58d4730e8437f6339bdcc98350e6f95d045
TonyWu20 <tony.w21@gmail.com>
2021-10-15 17:20:21 +0800

    Debugging mol

--- src/msiWriter/msiWriter.c
+++ src/msiWriter/msiWriter.c
@@ -3,7 +3,7 @@
 MSI_FILE *init_MSI_FILE(int ItemNum)
 {
     MSI_FILE *model;
-    model = malloc(sizeof(MSI_FILE) + ItemNum * 2 * sizeof(char));
+    model = malloc(sizeof(MSI_FILE) + ItemNum * 80 * sizeof(char));
     model->ItemNum = ItemNum;
     return model;
 }
@@ -13,7 +13,7 @@ MSI_FILE *build_MolMsi(MOLECULE *mol)
     int ItemNum = mol->atomNum + 3;
     MSI_FILE *mol_file = init_MSI_FILE(ItemNum);
     mol_file->lines[0] = "# MSI CERIUS2 DataModel File Version 4.0\n";
-    char *begin = "(1 Model\n";
+    char *begin = strdup("(1 Model\n");
     mol_file->lines[1] = begin;
     for (int i = 0; i < mol->atomNum; i++)
     {
@@ -25,7 +25,7 @@ MSI_FILE *build_MolMsi(MOLECULE *mol)
                  mol->molAtoms[i].coord[0], mol->molAtoms[i].coord[1],
                  mol->molAtoms[i].coord[2], mol->molAtoms[i].itemId - 1);
     }
-    mol_file->lines[mol->atomNum + 2] = ")";
+    mol_file->lines[mol->atomNum + 2] = strdup(")");
     return mol_file;
 }
 
