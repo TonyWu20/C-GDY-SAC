5a5c34b5a503f37fd323768a6be3c7d30fbc445f
TonyWu20 <tony.w21@gmail.com>
2022-01-31 12:53:36 +0800

    Split CastepInfo hash to castep_database

---
+++ src/castep_database.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "uthash.h"
+typedef struct
+{
+    const char *name;
+    struct ElmItem *info;
+    UT_hash_handle hh;
+} CastepInfo;
+
+struct ElmItem
+{
+    const char *elm;
+    int LCAO;
+    double mass;
+    const char *potential_file;
+    int spin;
+};
+void add_item(CastepInfo **table, struct ElmItem *item);
+
+CastepInfo *find_item(CastepInfo **table, const char *elm);
+
+void delete_all(CastepInfo **table);
+
+CastepInfo *initTable();
