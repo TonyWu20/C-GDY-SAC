6c24029aed067ba47a7dced6dcfd54736c7867a0
TonyWu20 <tony.w21@gmail.com>
2022-02-12 12:29:12 +0800

    Implemented `normalize_vector` and `cross_product`

--- src/my_maths.h
+++ src/my_maths.h
@@ -14,6 +14,7 @@
  */
 double *get_nth_vector_from_matrix(double *matrix, int rowSize, int colSize,
                                    int nth);
+
 /* Create a vector point from a to b
  *
  * arguments:
@@ -23,16 +24,25 @@ double *get_nth_vector_from_matrix(double *matrix, int rowSize, int colSize,
  * A malloced double * with size of vectorSize
  */
 double *create_vector_a_to_b(double *a, double *b, int vectorSize);
+
+/* normalized vector in place */
 void normalize_vector(double *vec);
+
 /* Call cblas_ddot to calculate dot_product of the xyz part */
 double dot_product(double *u, double *v);
+
 /* Calculate angle of two vector */
 double vector_angle(double *u, double *v);
-/* Matrix *cross_product(Matrix *a, Matrix *b); */
-/* Matrix *rotationMatrix(double rad, char axis); */
+
 /* Calculate centroid of the given set of vertices */
 double *centroid_of_points(double *coords, int rowSize, int colSize);
-/* Matrix *cross_product(Matrix *a, Matrix *b); // Return normalized vector */
+
+/* Return cross_product by matrix-vector multiplication
+ *
+ * returns:
+ * A malloced double * 1x4 vector, not normalized
+ */
+double *cross_product(double *a, double *b);
 
 /* Matrix *translate_mat_a_to_b(double *center_a, double *center_b); */
 /* void translate_a_to_b(Matrix *trans_mat, Matrix *coords, Matrix **result); */
