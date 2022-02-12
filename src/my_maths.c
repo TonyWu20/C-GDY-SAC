a7a3efeb7825f847c16cc1668c101a168f2775e7
TonyWu20 <tony.w21@gmail.com>
2022-02-12 23:16:14 +0800

    Implemented with `simd`

--- src/my_maths.c
+++ src/my_maths.c
@@ -1,5 +1,4 @@
 #include "my_maths.h"
-#include "matrix.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,47 +22,30 @@ double *get_nth_vector_from_matrix(double *matrix, int rowSize, int colSize,
     return vec;
 }
 
-double *create_vector_a_to_b(double *a, double *b, int vectorSize)
+simd_double4 get_simd_double4_from_matrix(double *matrix, int rowSize,
+                                          int colSize, int i)
 {
-    double *c = malloc(sizeof(double) * vectorSize);
-    memcpy(c, b, sizeof(double) * vectorSize);
-    cblas_daxpy(3, -1, a, 1, c, 1);
-    return c;
-}
-
-void normalize_vector(double *u)
-{
-    double norm = cblas_dnrm2(3, u, 1);
-    cblas_dscal(3, 1 / norm, u, 1);
-}
-
-double dot_product(double *u, double *v)
-{
-    double ret = cblas_ddot(3, u, 1, v, 1);
-    return ret;
-}
-
-double vector_angle(double *u, double *v)
-{
-    double dotProduct = dot_product(u, v);
-    double u_norm = cblas_dnrm2(3, u, 1);
-    double v_norm = cblas_dnrm2(3, v, 1);
-    double angle = acos(dotProduct / (u_norm * v_norm));
-    return angle;
-}
-
-double *centroid_of_points(double *coords, int rowSize, int colSize)
-{
-    /* ColMajor */
-    double avg_vec[colSize];
-    for (int i = 0; i < colSize; ++i)
+    simd_double4 vec = simd_double((simd_double4){0, 0, 0, 0});
+    if (i >= rowSize)
     {
-        avg_vec[i] = (double)1 / (double)colSize;
+        printf("Slice out of matrix range.\n");
+        return vec;
     }
-    double *centroid = calloc(4, sizeof(double));
-    cblas_dgemv(CblasColMajor, CblasNoTrans, rowSize, colSize, 1, coords,
-                rowSize, avg_vec, 1, 0, centroid, 1);
-    return centroid;
+    vec = simd_double(
+        (simd_double4){matrix[i * colSize + 0], matrix[i * colSize + 1],
+                       matrix[i * colSize + 2], matrix[i * colSize + 3]});
+    return vec;
+}
+
+simd_double3 simd_centroid_of_points(simd_double3 *vecArray, int vecArraySize)
+{
+    simd_double3 res = {0, 0, 0};
+    for (int i = 0; i < vecArraySize; ++i)
+    {
+        res += vecArray[i];
+    }
+    res = res / (double)vecArraySize;
+    return res;
 }
 
 double *cross_product(double *a, double *b) // Return normalized vector
@@ -78,16 +60,59 @@ double *cross_product(double *a, double *b) // Return normalized vector
 }
 
 // Determine the transform matrix to make u parallel to v
-double *rotate_u_to_v(double *u, double *v)
+
+void rotate_coords_around_axis_by_angle(double *axis, double rad,
+                                        double *coords, int rowSize,
+                                        int colSize, double **result)
 {
+    normalize_vector(axis);
+    double ux, uy, uz;
+    ux = axis[0];
+    uy = axis[1];
+    uz = axis[2];
+    double a = cos(rad);
+    double b = 1 - a;
+    double c = sin(rad);
+    double R_axis_rad[] = {a + ux * ux * b,
+                           uy * ux * b + uz * c,
+                           uz * ux * b + uy * c,
+                           0, //
+                           ux * uy * b - uz * c,
+                           a + uy * uy * b,
+                           uz * uy * b + ux * c,
+                           0, //
+                           ux * uz * b + uy * c,
+                           uy * uz * b - ux * c,
+                           a + uz * uz * b,
+                           0,
+                           0,
+                           0,
+                           0,
+                           1};
+    *result = calloc(rowSize * colSize, sizeof(double));
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, rowSize, colSize, 4,
+                1, R_axis_rad, 4, coords, 4, 0, *result, 4);
+    /* return result; */
 }
 
-double *rotate_angle_around_axis(double *axis, double rad)
+simd_double4x4 simd_rotationMatrix(simd_double3 axis, double rad)
 {
-}
-
-double *translate_mat_a_to_b(double *src, double *dest)
-{
+    axis = simd_normalize(axis);
+    double a = cos(rad);
+    double b = 1 - cos(rad);
+    double c = sin(rad);
+    double ux = axis.x;
+    double uy = axis.y;
+    double uz = axis.z;
+    simd_double4 x_basis = {a + ux * ux * b, uy * ux * b + uz * c,
+                            uz * ux * b - uy * c, 0};
+    simd_double4 y_basis = {uy * ux * b - uz * c, a + uy * uy * b,
+                            uz * uy * b + ux * c, 0};
+    simd_double4 z_basis = {uz * ux * b + uy * c, uz * uy * b - ux * c,
+                            a + uz * uz * b, 0};
+    simd_double4 t_basis = {0, 0, 0, 1};
+    simd_double4x4 rotMat = {x_basis, y_basis, z_basis, t_basis};
+    return rotMat;
 }
 
 int roundupBiggerTenth(int number)
