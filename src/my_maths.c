7f79a110f00e1ac7ab6270943dc49313ae422f58
TonyWu20 <tony.w21@gmail.com>
2022-02-16 23:19:45 +0800

    Remove Accelerate

--- src/my_maths.c
+++ src/my_maths.c
@@ -5,44 +5,6 @@
 #include <string.h>
 #define PI (atan(1) * 4)
 
-double *get_nth_vector_from_matrix(double *matrix, int rowSize, int colSize,
-                                   int nth)
-{
-    double *vec = NULL;
-    if (nth >= rowSize)
-    {
-        printf("Slice out of matrix range.\n");
-        return vec;
-    }
-    vec = malloc(sizeof(double) * colSize);
-    for (int i = 0; i < colSize; ++i)
-    {
-        vec[i] = matrix[nth * colSize + i];
-    }
-    return vec;
-}
-
-double *create_vector_a_to_b(double *a, double *b, int vectorSize)
-{
-    double *c = malloc(sizeof(double) * vectorSize);
-    memcpy(c, b, sizeof(double) * vectorSize);
-    cblas_daxpy(3, -1, a, 1, c, 1);
-    return c;
-}
-
-double dot_product(double *u, double *v)
-{
-    return cblas_ddot(3, u, 1, v, 1);
-}
-
-double vector_angle(double *u, double *v)
-{
-    double dP = dot_product(u, v);
-    double u_norm = sqrt(dot_product(u, u));
-    double v_norm = sqrt(dot_product(v, v));
-    return acos(dP / (u_norm * v_norm));
-}
-
 double simd_vector_angle(simd_double3 u, simd_double3 v)
 {
     double dP_uv = simd_dot(u, v);
@@ -109,72 +71,7 @@ simd_double4x4 translateMatrix(simd_double3 fromVec, simd_double3 toVec)
     return transMat;
 }
 
-double *cross_product(double *a, double *b) // Return normalized vector
-{
-    double a_skew_mat[16] = {0,     a[2],  -a[1], 0, //
-                             -a[2], 0,     a[0],  0, //
-                             a[1],  -a[0], 0,     0, 0, 0, 0, 1};
-    double *cProduct = calloc(4, sizeof(double));
-    cblas_dgemv(CblasColMajor, CblasNoTrans, 4, 4, 1, a_skew_mat, 4, b, 1, 0,
-                cProduct, 1);
-    return cProduct;
-}
-
-double *centroid_of_points(double *coords, int rowSize, int colSize)
-{
-    double avg_vec[colSize];
-    for (int i = 0; i < colSize; ++i)
-    {
-        avg_vec[i] = 1 / (double)colSize;
-    }
-    double *centroid = calloc(4, sizeof(double));
-    cblas_dgemv(CblasColMajor, CblasNoTrans, rowSize, colSize, 1, coords,
-                rowSize, avg_vec, 1, 0, centroid, 1);
-    return centroid;
-}
-
-void normalize_vector(double *vec)
-{
-    double norm = sqrt(cblas_ddot(3, vec, 1, vec, 1));
-    cblas_dscal(3, 1 / norm, vec, 1);
-}
-
 // Determine the transform matrix to make u parallel to v
-
-void rotate_coords_around_axis_by_angle(double *axis, double rad,
-                                        double *coords, int rowSize,
-                                        int colSize, double **result)
-{
-    normalize_vector(axis);
-    double ux, uy, uz;
-    ux = axis[0];
-    uy = axis[1];
-    uz = axis[2];
-    double a = cos(rad);
-    double b = 1 - a;
-    double c = sin(rad);
-    double R_axis_rad[] = {a + ux * ux * b,
-                           uy * ux * b + uz * c,
-                           uz * ux * b + uy * c,
-                           0, //
-                           ux * uy * b - uz * c,
-                           a + uy * uy * b,
-                           uz * uy * b + ux * c,
-                           0, //
-                           ux * uz * b + uy * c,
-                           uy * uz * b - ux * c,
-                           a + uz * uz * b,
-                           0,
-                           0,
-                           0,
-                           0,
-                           1};
-    *result = calloc(rowSize * colSize, sizeof(double));
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, rowSize, colSize, 4,
-                1, R_axis_rad, 4, coords, 4, 0, *result, 4);
-    /* return result; */
-}
-
 int roundupBiggerTenth(int number)
 {
     int rounded = (number / 10 + 1) * 10;
--- src/my_maths.h
+++ src/my_maths.h
@@ -1,54 +1,17 @@
 #pragma once
-#include <Accelerate/Accelerate.h>
 #include <math.h>
 #include <simd/simd.h>
 
 #define PI (atan(1) * 4)
 
-/* Get the ith vector from the mxn matrix as with the dimension of 1xn
- *
- * arguments:
- * double *matrix, int rowSize, int colSize, int nth (start from 0)
- *
- * returns:
- * A malloced double * with size of colSize
- */
-double *get_nth_vector_from_matrix(double *matrix, int rowSize, int colSize,
-                                   int nth);
 simd_double4 get_simd_double4_from_matrix(double *matrix, int rowSize,
                                           int colSize, int i);
-
-/* Create a vector point from a to b
- *
- * arguments:
- * double *a, double *b, int vectorSize
- *
- * returns:
- * A malloced double * with size of vectorSize
- */
-double *create_vector_a_to_b(double *a, double *b, int vectorSize);
-
-/* normalized vector in place */
-void normalize_vector(double *vec);
-
-/* Call cblas_ddot to calculate dot_product of the xyz part */
-double dot_product(double *u, double *v);
-
-/* Calculate angle of two vector */
-double vector_angle(double *u, double *v);
 
 double simd_vector_angle(simd_double3 u, simd_double3 v);
 
 /* Calculate centroid of the given set of vertices */
-double *centroid_of_points(double *coords, int rowSize, int colSize);
 
 simd_double3 simd_centroid_of_points(simd_double3 *vecArray, int vecArraySize);
-/* Return cross_product by matrix-vector multiplication
- *
- * returns:
- * A malloced double * 1x4 vector, not normalized
- */
-double *cross_product(double *a, double *b);
 
 /* Matrix *translate_mat_a_to_b(double *center_a, double *center_b); */
 /* void translate_a_to_b(Matrix *trans_mat, Matrix *coords, Matrix **result); */
