f4c401315d66b7bbd6bf67d594be3c8fe309be2e
TonyWu20 <tony.w21@gmail.com>
2022-05-04 12:13:34 +0800

    Clean unnecessary hardcoded arrays of variables

--- src/tasks.c
+++ src/tasks.c
@@ -10,15 +10,8 @@
 #include <unistd.h>
 /* static char *findBaseByElementId(int i); */
 
-#define TOTAL_ELEMENT_NUM 44
+#define TOTAL_ELEMENT_NUM 1
 #define TOTAL_MODELS (231 * TOTAL_ELEMENT_NUM)
-enum
-{
-    T3D,
-    T4D,
-    T5D,
-    LM
-};
 
 enum
 {
@@ -36,74 +29,8 @@ int task_cd[][2] = {{C1, C2}, {C2, C3}, {C3, C4}, {C4, FR},
 int task_cd1[][2] = {{C1, NULLSITE}, {C2, NULLSITE}, {C3, NULLSITE},
                      {C4, NULLSITE}, {NR, NULLSITE}, {FR, NULLSITE},
                      {M, NULLSITE}};
-char *pathways[] = {"ethylene", "acetic_acid", "ethanol", "ethanol_other_ads"};
-char *ethylene_ads[] = {
-    "CO.msi",      "CHO.msi",      "COCHO.msi",  "COCHOH.msi",  "OCH2CO.msi",
-    "OCH2COH.msi", "OCH2CHOH.msi", "OCH2CH.msi", "OCH2CH2.msi", "C2H4.msi"};
-char *acetic_acid_ads[] = {"OCH2C_cyc_OH.msi", "CH3COOH.msi"};
-char *ethanol_ads[] = {
-    "Glyoxal.msi",        "HOCCHO.msi",   "HOHCCHO.msi",
-    "Glycolaldehyde.msi", "CH2CHO.msi",   "CH2CHOH.msi",
-    "CH2CH2OH.msi",       "CH3CH2OH.msi", "acetaldehyde.msi"};
-char *ethanol_other_ads[] = {"CH2OHCH2O.msi", "ethylene_glycol.msi"};
 
-double heightChoice[] = {1.4, 1.6, 1.4, 1.4};
-
-Adsorbate *getAds_from_i(int i, char **adsList, int adsListLen,
-                         HashNode *adsTable)
-{
-    int currAds = i % adsListLen;
-    char *ads_name = extractStemName(adsList[currAds]);
-    AdsInfo *info = (AdsInfo *)find_item_by_str(adsTable, ads_name)->val;
-    Adsorbate *ads =
-        parse_adsorbate_from_file(adsList[currAds], ads_name, info);
-    free(ads_name);
-    return ads;
-}
-
-char **pathway_adsLists(int pathNameCode, int *adsListLen)
-{
-    *adsListLen = 0;
-    char **list = NULL;
-    switch (pathNameCode)
-    {
-    case ETHYLENE:
-        *adsListLen = sizeof(ethylene_ads) / sizeof(char *);
-        list = adsListBuild("ethylene", ethylene_ads, *adsListLen);
-        break;
-    case ACETIC_ACID:
-        *adsListLen = sizeof(acetic_acid_ads) / sizeof(char *);
-        list = adsListBuild("acetic_acid", acetic_acid_ads, *adsListLen);
-        break;
-    case ETHANOL:
-        *adsListLen = sizeof(ethanol_ads) / sizeof(char *);
-        list = adsListBuild("ethanol", ethanol_ads, *adsListLen);
-        break;
-    case ETHANOL_OTHER:
-        *adsListLen = sizeof(ethanol_other_ads) / sizeof(char *);
-        list = adsListBuild("ethanol_other", ethanol_other_ads, *adsListLen);
-        break;
-    default:
-        break;
-    }
-    return list;
-}
-
-char **adsListBuild(char *pathName, char *adsList[], int listLen)
-{
-    char **list = malloc(sizeof(char *) * listLen);
-    for (int i = 0; i < listLen; ++i)
-    {
-        int filePathLen = 1 + snprintf(NULL, 0, "./C2_pathways_ads/%s_path/%s",
-                                       pathName, adsList[i]);
-        list[i] = malloc(filePathLen);
-        snprintf(list[i], filePathLen, "./C2_pathways_ads/%s_path/%s", pathName,
-                 adsList[i]);
-    }
-    return list;
-}
-
-Adsorbate *getCurrendAds(AdsTableYAML *adsTableYAML, int adsId)
+Adsorbate *getCurrentAds(AdsTableYAML *adsTableYAML, int adsId)
 {
     AdsInfo curAdsInfo = adsTableYAML->adsInfoItem[adsId];
     char *path;
@@ -156,7 +83,7 @@ void generator(ElmTableYAML *elmTableYAML, char **elements,
                     int nextElmId = ((ElmInfo *)(nextElm->val))->atomicNum;
                     base->vtable->modify_metal(base, nextElm->key,
                                                 nextElmId);
-                    Adsorbate *ads = getCurrendAds(adsTableYAML, j);
+                    Adsorbate *ads = getCurrentAds(adsTableYAML, j);
                     switch (ads->coordAtomNum)
                     {
                     case 2:
